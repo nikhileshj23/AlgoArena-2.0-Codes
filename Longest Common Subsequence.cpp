f6f92807a38afab47b314a6aed103717e8798ee3
nikhileshj23 <148950594+nikhileshj23@users.noreply.github.com>
2024-06-05 22:55:55 +0530

    Update Longest Common Subsequence.cpp

--- Longest Common Subsequence.cpp
+++ Longest Common Subsequence.cpp
@@ -53,16 +53,33 @@ void solve(){
     string a,b;
     cin>>a>>b;
     int n=a.size(),m=b.size();
-    int dp[n+1][m+1];
+    int dp[m+1][n+1];
     for(int i=0;i<n+1;i++){
         for(int j=0;j<m+1;j++) dp[i][j]=0;
     }
-    for(int i=1;i<n+1;i++){
-        for(int j=1;j<m+1;j++){
-            dp[i][j]=max(dp[i-1][j],max(dp[i][j-1],dp[i-1][j-1]+(a[i-1]==b[j-1])));
+    for(int i=1;i<m+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(a[j-1]==b[i-1]) dp[i][j]=dp[i-1][j-1]+1;
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
         }
     }
-    cout<<dp[n][m]<<endl;
+    
+    string s;
+    int i=m,j=n;
+    while(i>0&&j>0){
+        if(a[j-1]==b[i-1]){
+            s.pb(a[j-1]);
+            i--;
+            j--;
+            
+        }else if(dp[i-1][j]>dp[i][j-1]){
+            i--;
+        }else j--;
+        
+    }
+    reverse(all(s));
+    cout<<s<<endl;
+    cout<<dp[m][n]<<endl;
 }
  
 int main(){
