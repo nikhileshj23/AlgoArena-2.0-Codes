1f6c4daec34e6a986d4d168549764bcec6c425a2
nikhileshj23 <148950594+nikhileshj23@users.noreply.github.com>
2024-06-06 17:42:35 +0530

    Create Minimizing Coins.cpp

---
+++ Minimizing Coins.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<fstream>
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define FOR(i,b) for(int i=0;i<b;i++)
+#define FORwi(i,a,b) for(int i=a;i<b;i++)
+#define get(v) FOR(i,v.size()) cin>>v[i];
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define see(v) FOR(i,sz(v)) cout<<v[i]<<" "; cout<<endl;
+#define gt(v) for(auto it=v.begin();it!=v.end();it++)
+#define sz(v) v.size()
+#define vi vector<int>
+#define vll vector<ll>
+#define mpii map<int,int> 
+#define mpll map<ll,ll> 
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<ll,ll>
+typedef long long int ll;
+typedef unsigned long long ull;
+int MOD=1000000007;
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+ 
+int prime(long long int a){
+    if(a==1) return 0;
+    for(int i=2;i<=sqrt(a);i++){
+        if(a%i==0) return 0;
+    }
+    return 1;
+}
+ 
+string base_rep(ll n, ll b){  //for representation in any base
+    if(n==0) return "0";
+    string res="";
+    while(n>0){
+        res+=char('0'+n%b);
+        n=n/b;
+    }
+    reverse(all(res));
+    return res;
+}
+ 
+void solve(){
+    ll n,x;
+    cin>>n>>x;
+    vll v(n);get(v);
+    vll dp(x+1,-1);
+    dp[0]=0;
+    for(int i=0;i<n;i++){
+        if(v[i]<=x) dp[v[i]]=1;
+    }
+
+    for(int i=1;i<=x;i++){
+        ll mini=INT_MAX;
+        for(int j=0;j<n;j++){
+            if(v[j]<=i){
+                if(dp[i-v[j]]!=-1) mini=min(mini,dp[i-v[j]]+1);
+            }
+        }
+        if(mini!=INT_MAX) dp[i]=mini;
+    }
+    
+    cout<<dp[x]<<endl;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL); 
+    
+    // freopen("helpcross.in", "r", stdin);
+	// freopen("helpcross.out", "w", stdout);
+ 
+ 
+    solve();
+    
+    // int t;
+    // cin>>t;
+    // while(t--){
+    //     solve();
+    // }
+}
