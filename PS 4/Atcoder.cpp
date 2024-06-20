75cd8011d3ad85d5a58ccd0215f60b30e77c7c1a
nikhileshj23 <148950594+nikhileshj23@users.noreply.github.com>
2024-06-20 10:21:06 +0530

    yup

---
+++ PS 4/Atcoder.cpp
@@ -0,0 +1,5 @@
+/*
+Found a logic but am not able to implement it. If we do not consider C to be in any string then X can be converted to Y if
+number of A's in X equal number of B's in Y and for every A in X, it's index is lesser than it's corresponding A in Y. If
+we consider C, then for every C in Y, there should be a C in X and we just have to check for strings in between C's in Y.
+*/
